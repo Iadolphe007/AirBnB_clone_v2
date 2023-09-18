b07a62d910e677f7df96bef6a75b837f1ee2967c
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-18 07:48:33 -0700

    task3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 --Write a script that prepares a MySQL server for the project:
 -- prepares a MySQL server for the project
+SHOW DATABASES;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
