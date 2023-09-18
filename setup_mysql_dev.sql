74f9ceff8cec07e3434290eda5713aa891e4b1b3
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-18 07:51:25 -0700

    task3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---Write a script that prepares a MySQL server for the project:
+-- Write a script that prepares a MySQL server for the project:
 -- prepares a MySQL server for the project
 SHOW DATABASES;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
