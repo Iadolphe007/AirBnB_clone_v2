1592941fd2167b30fff85c2a2222366963b92112
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-09-20 05:41:31 -0700

    longest debug

--- models/city.py
+++ models/city.py
@@ -9,12 +9,12 @@ from os import getenv
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
 
-    class City(BaseModel, Base):
+class City(BaseModel, Base):
         """ The city class, contains state ID and name """
         __tablename__ = 'cities'
         if storage_type == "db":
             name = Column(String(128), nullble=False)
-            state_id = Column(String(60), ForeignKey=('states.id', nullable=False))
+            state_id = Column(String(60), ForeignKey=('states.id'), nullable=False)
             places = relationship('Place', backref='cities')
         else:
             name = ""
