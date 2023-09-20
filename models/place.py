6d70ecfcee85be02ec369099bf9fc7ec53a39223
IMANISHIMWE ADOLPHE <114081731+Iadolphe007@users.noreply.github.com>
2023-09-20 14:52:02 +0200

    Updated place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy.orm import relationship
 
+metadata = Base.metadata 
+place_amenity = Table("place_amenity", metadata,
+                      Column("place_id", String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False))
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class Place(BaseModel):
+    class Place(BaseModel, Base):
         """ A place to stay """
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey(
+            'cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey(
+            'users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
@@ -21,15 +34,15 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place')
         amenities = relationship(
-                'Amenity',secondary='place_amenity',viewonly=False)
+            'Amenity',secondary='place_amenity',viewonly=False)
 
 else:
     class Place(BaseModel):
-        """ A place to stay """
-        city_id = ''
-        user_id = ''
-        name = ''
-        description = ''
+        """Place Model"""
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
         number_rooms = 0
         number_bathrooms = 0
         max_guest = 0
