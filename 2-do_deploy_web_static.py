33bc5c03a255dbb4138c61f4bc8c028937a99a3b
IMANISHIMWE ADOLPHE <114081731+Iadolphe007@users.noreply.github.com>
2023-10-07 21:42:40 +0200

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
+
 from fabric.api import local, settings, abort, run, cd, env, put, get
 from fabric.decorators import task, hosts, with_settings
 from os import path
 from datetime import datetime
 
-env.hosts = ['52.86.110.104', '34.204.95.100']
-env.user = 'ubuntu'
 
+env.hosts = ["34.204.95.100", "52.86.110.104"]
+env.user = "ubuntu"
 
 def do_pack():
-    """
-    Generates a .tgz archive from the
-    contents of the web_static folder
-    Returns:
-        Path to the archive if successful, None otherwise
-    """
+    """function"""
     local("mkdir -p versions")
-    name = "web_static_{}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
-    ruta = "versions/{}".format(name)
-    local("tar czfv versions/{}.tgz web_static".format(name))
+    timestamp= "web_static_{}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    pathto = "versions/{}".format(timestamp)
+    local("tar czfv versions/{}.tgz web_static".format(timestamp))
 
-    if path.isfile(ruta):
-        return ruta
+    if path.isfile(pathto):
+        return pathto
     else:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Distribute an archive to web servers
-    Args:
-        archive_path: Path to the archive file on the local machine
-    Returns:
-        True if all operations are successful, otherwise False
-    """
+    """function"""
     if path.isfile(archive_path):
         name_file = archive_path[9:]
         new_path = "/data/web_static/releases/"
@@ -52,8 +42,6 @@ def do_deploy(archive_path):
         print("New version deployed!")
         return True
     return False
-
-
 def deploy():
     """creates"""
     archive_path = do_pack()
