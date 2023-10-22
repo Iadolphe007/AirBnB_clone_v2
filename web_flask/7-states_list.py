606d6daaa3c6d22e6bcdf7a8ed372ce4e1a084db
Iadolphe007 <iadolphe007@gmail.com>
2023-10-22 11:56:26 +0000

    sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """list all state"""
-    states = storage.all(State).value()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
